a11709b947d2f1b19cc55817a3cadef7c0b6a798
achraf-developer <technichome.achraf@gmail.com>
2023-06-10 12:31:00 +0100

    add new section and start slider fonctionalite

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centred:true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type:"fraction"
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+    breakpoints:{
+        200: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+    }
+  });
